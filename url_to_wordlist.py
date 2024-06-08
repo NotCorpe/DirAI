6e9c3aacd16785982b4484dcc4156af143069d54
NotCorpe <133766493+NotCorpe@users.noreply.github.com>
2024-06-08 15:26:31 +0200

    Update and rename discoveria.py to url_to_wordlist.py

--- url_to_wordlist.py
+++ url_to_wordlist.py
@@ -7,10 +7,10 @@ from urllib.parse import urljoin
 import argparse
 
 class WebsiteSpider:
-    def __init__(self, start_url, api_key, max_depth=3):
+    def __init__(self, start_url, language, max_depth=3):
         self.start_url = start_url
         self.max_depth = max_depth
-        self.api_key = api_key
+        self.language = language
         self.total_words = 0
         self.keywords = []
         self.visited_urls = set()
@@ -18,27 +18,22 @@ class WebsiteSpider:
     def parse(self, url, depth=0):
         if depth > self.max_depth:
             return
-
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         text = soup.get_text()
         self.total_words += len(text.split())
         print(f"Number of words found on {url}: {len(text.split())}")
-
-        language = langdetect.detect(text)
-
-        if language == 'fr':
+        if self.language == 'fr':
             nlp = spacy.load("fr_core_news_sm")
-        elif language == 'en':
+        elif self.language == 'en':
             nlp = spacy.load("en_core_web_sm")
         else:
+            print(f"Language '{self.language}' not supported.")
             return
-
         doc = nlp(text)
         words = [token.text for token in doc if not token.is_stop and not token.is_punct and not token.is_space]
         word_counts = Counter(words)
         self.keywords.extend(word_counts.keys())
-
         for link in soup.find_all('a'):
             href = link.get('href')
             if href and href.startswith(self.start_url):
@@ -50,13 +45,10 @@ class WebsiteSpider:
     def close(self):
         print(f"Total number of words found: {self.total_words}")
         print(f"Total number of keywords extracted: {len(self.keywords)}")
-
         sorted_keywords = sorted(set(self.keywords))
-
         with open('keywords.txt', 'w') as f:
             for keyword in sorted_keywords:
                 f.write(f"{keyword}\n")
-
         print("Visited pages:")
         for url in self.visited_urls:
             print(url)
@@ -64,15 +56,12 @@ class WebsiteSpider:
 def main():
     parser = argparse.ArgumentParser(description='Website Spider')
     parser.add_argument('--url', '-u', required=True, help='Start URL for the spider')
+    parser.add_argument('--language', '-l', default='fr', help='Language of the website (default: fr)')
     args = parser.parse_args()
-
     start_url = args.url
+    language = args.language
     max_depth = 2
-
-    with open('api_key.txt', 'r') as f:
-        api_key = f.read().strip()
-
-    spider = WebsiteSpider(start_url, api_key, max_depth)
+    spider = WebsiteSpider(start_url, language, max_depth)
     spider.parse(start_url)
     spider.close()
 
